6c50752abb940a8de94289eb4fbd2aff7aeb775e
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2024-09-07 14:20:42 +0200

    Add admin login page

---
+++ client/src/stores/auth.ts
@@ -0,0 +1,29 @@
+import router from '@/router';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+interface User {
+    username: string;
+}
+
+export const useAuthStore = defineStore('auth', () => {
+    const isAuthenticated = ref(false);
+    const user = ref<User | null>(null);
+
+    function login(username: string, password: string) {
+        if (username === 'admin' && password === 'password') {
+            isAuthenticated.value = true;
+            user.value = { username };
+
+            router.replace({ name: 'admin' });
+        } else {
+            throw new Error('Invalid credentials');
+        }
+    }
+
+    return {
+        isAuthenticated,
+        user,
+        login,
+    };
+});
---
+++ client/src/views/AdminLoginView.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
+import { ref } from 'vue';
+
+const auth = useAuthStore();
+
+const username = ref('');
+const password = ref('');
+
+</script>
+
+<template>
+    <div class="admin-login">
+        <h1>Admin Login</h1>
+        <form @submit.prevent="auth.login(username, password)">
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+.admin-login {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+.admin-login h1 {
+    text-align: center;
+}
+
+.admin-login form {
+    display: flex;
+    flex-direction: column;
+}
+
+.admin-login label {
+    margin-bottom: 10px;
+}
+
+.admin-login input {
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+.admin-login button {
+    padding: 10px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+
+.admin-login button:hover {
+    background-color: #0056b3;
+}
+</style>
